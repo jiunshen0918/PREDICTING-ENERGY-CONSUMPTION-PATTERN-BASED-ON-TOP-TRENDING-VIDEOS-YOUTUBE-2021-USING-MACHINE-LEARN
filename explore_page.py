c9a6bb5577bed048789db76ccf2bcf4e621b3309
jiunshen0918 <79739744+jiunshen0918@users.noreply.github.com>
2022-08-01 01:15:40 +0800

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -1,3 +1,12 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+
+def show_predict_page():
+    st.title("Youtube Energy Estimate Page")
+
+    st.write("""### Welcome""")
+    
+    ok = st.button("Show energy consumption pattern")
+    
+show_predict_page():
