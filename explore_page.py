95fa5ab23b65ec61a76f9b631ac77ecd916fdaa4
jiunshen0918 <79739744+jiunshen0918@users.noreply.github.com>
2022-08-01 07:19:05 +0800

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -3,8 +3,6 @@ import numpy as np
 import pandas as pd
 
 #define explore coding
-
-
 def show_explore_page():
     st.title("Youtube Energy Estimate Page")
 
@@ -16,6 +14,9 @@ def show_explore_page():
     #buttons
     head = st.button("Show top 5 records of data")
     tail = st.button("Show last 5 records of data")
+    
+    if head:
+        st.write("""Top 5 records of data is""",youtube.head())
     
 show_explore_page()
     
