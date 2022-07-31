cc1a4fbe149e17951809dcf6906197cb60c0444c
jiunshen0918 <79739744+jiunshen0918@users.noreply.github.com>
2022-08-01 07:15:14 +0800

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -1,11 +1,21 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+
+#define explore coding
+
 
 def show_explore_page():
     st.title("Youtube Energy Estimate Page")
 
     st.write("""### Welcome""")
     
+    #load data
+    youtube=pd.read_csv("YoutubeDataset.csv")
+    
+    #buttons
     head = st.button("Show top 5 records of data")
+    tail = st.button("Show last 5 records of data")
     
 show_explore_page()
     
