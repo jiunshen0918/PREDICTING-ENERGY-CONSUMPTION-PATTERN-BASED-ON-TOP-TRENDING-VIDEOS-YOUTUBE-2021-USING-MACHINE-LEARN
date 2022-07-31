1a589f75c91ce2c1699e8cf659722af4a00cc26f
jiunshen0918 <79739744+jiunshen0918@users.noreply.github.com>
2022-08-01 06:53:53 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from explore_page import show_explore_page
+
+page = st.sidebar.selectionbox("Explore or Predict", ("Explore","Predict"))
+
+if page == "Explore":
+  show_explore_page()
+else
+  show_predict_page()
