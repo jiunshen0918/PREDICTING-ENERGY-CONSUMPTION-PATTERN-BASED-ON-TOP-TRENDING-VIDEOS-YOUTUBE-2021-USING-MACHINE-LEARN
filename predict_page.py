353f0974c3763bb26b778a89e89814f204717575
jiunshen0918 <79739744+jiunshen0918@users.noreply.github.com>
2022-07-31 16:52:16 +0800

    Create predict_page.py

---
+++ predict_page.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pickle 
+import numpy as np
+
+def load_model():
+    with open('saved.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+data = load_model()
+
+lin_reg_loaded = data["model"]
+totalenergy = data["totalenergy"]
+views = data["views"]
+
+def show_predict_page():
+    st.title("Youtube Energy Estimate Page")
+
+    st.write("""### We need some information""")
