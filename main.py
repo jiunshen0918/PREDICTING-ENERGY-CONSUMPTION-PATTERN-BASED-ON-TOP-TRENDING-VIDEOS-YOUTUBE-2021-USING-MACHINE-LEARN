a5e37fe64bc49e743a29edb6143b628acc0e1ebc
jiunshen0918 <79739744+jiunshen0918@users.noreply.github.com>
2022-08-01 01:01:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from predict_page import show_predict_page
 
-st.sidebar.selection("Explore or prediction",("Predict","Explore"))
+st.sidebar.selectbox("Explore or prediction",("Predict","Explore"))
 
 show_predict_page()
