03d0809621d7374cd8092c31083729d078f32776
jiunshen0918 <79739744+jiunshen0918@users.noreply.github.com>
2022-08-04 07:33:22 +0800

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -24,11 +24,9 @@ def show_explore_page():
     elif tail:
         st.write("""Last 5 records of data is""",youtube.tail())
     elif info: 
-        show = pd.DataFrame(youtube)
         buffer = io.StringIO() #to store as memory file-like object
-        show.info(buf=buffer) #info is a standard file object
+        youtube.info(buf=buffer) #info is a standard file object
         st.write("""Data's information is""",buffer.getvalue())
-        st.info(youtube.info)
     elif desc:
         st.write("""Data's description is""",youtube.describe())
     elif corr:
