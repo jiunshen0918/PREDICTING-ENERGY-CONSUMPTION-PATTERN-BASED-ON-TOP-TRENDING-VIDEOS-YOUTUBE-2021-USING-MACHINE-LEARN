c9b40ab41e9a164844f244ef0d0e7f67e0fba466
jiunshen0918 <79739744+jiunshen0918@users.noreply.github.com>
2022-08-04 10:00:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,6 @@ from explore_page import show_explore_page
 from predict_page import show_predict_page
 
 def sidebar():
-  st.markdown(
-  """
-  <style>
-  .sidebar {
-    background-image: linear-gradient(#2e7bcf,#2e7bcf);
-    color: blue;
-  }
-  </style>
-  """,
-    unsafe_allow_html=True,
-  )
-    
   page = st.sidebar.selectbox("Explore or Predict", ("Explore","Predict"))
   st.sidebar.image("bongo-cat.gif")
   return page
