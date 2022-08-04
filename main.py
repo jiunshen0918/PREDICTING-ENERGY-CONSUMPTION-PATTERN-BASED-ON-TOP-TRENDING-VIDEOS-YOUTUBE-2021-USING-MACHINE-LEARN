12c2e4530c6050b71c9a7af370ee33633da547f7
jiunshen0918 <79739744+jiunshen0918@users.noreply.github.com>
2022-08-04 09:51:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from explore_page import show_explore_page
 from predict_page import show_predict_page
 
 def sidebar():
-  st.sidebar.image("bongo-cat.gif", width = 300)
+  st.sidebar.image("bongo-cat.gif")
   page = st.sidebar.selectbox("Explore or Predict", ("Explore","Predict"))
   return page
 
