5f418a8f24a126a457099586e3476d11a72ad651
jiunshen0918 <79739744+jiunshen0918@users.noreply.github.com>
2022-08-04 08:43:51 +0800

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -45,8 +45,19 @@ def show_predict_page():
     st.title("Youtube Energy Estimate Page")
 
     st.write("""### Predict the *Data!* :smile:""")
+	
+    # Load gif file
+    file_ = open("C:/Users/admin/OneDrive/FYP/UCCC3596-FYP2/bongocat.gif", "rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
+
+    st.markdown(
+        f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+        unsafe_allow_html=True,
+    )
     
-    # Find optimal
+    # Load data
     X,y = load_data()
     
     mylinreg = MyLinearRegression()
