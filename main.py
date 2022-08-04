77389f313a52e4bc63ac2bfd5b802183f612ffe1
jiunshen0918 <79739744+jiunshen0918@users.noreply.github.com>
2022-08-04 08:42:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,4 @@ if page == "Explore":
   show_explore_page()
 elif page == "Predict":
   show_predict_page()
-  
-"""### gif from local file"""
-file_ = open("C:/Users/admin/OneDrive/FYP/UCCC3596-FYP2/bongocat.gif", "rb")
-contents = file_.read()
-data_url = base64.b64encode(contents).decode("utf-8")
-file_.close()
-
-st.markdown(
-    f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
-    unsafe_allow_html=True,
-)
+ 
