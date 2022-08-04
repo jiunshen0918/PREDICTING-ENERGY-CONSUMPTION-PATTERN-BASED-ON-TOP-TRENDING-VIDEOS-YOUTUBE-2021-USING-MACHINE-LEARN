eb0987eee75b63ca00309c310c28a54d5baa4846
jiunshen0918 <79739744+jiunshen0918@users.noreply.github.com>
2022-08-04 09:45:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ import streamlit as st
 from explore_page import show_explore_page
 from predict_page import show_predict_page
 
-page = st.sidebar.selectbox("Explore or Predict", ("Explore","Predict"))
+def _sidebar(self):
+    st.sidebar.selectbox("Explore or Predict", ("Explore","Predict"))
+    """Format sidebar."""
+    st.sidebar.image(
+        "https://github.com/compomics/deeplc/raw/master/img/deeplc_logo.png",
+        width=150,
+    )
+    st.sidebar.markdown(self.texts.Sidebar.badges)
+    st.sidebar.header("About")
+    st.sidebar.markdown(self.texts.Sidebar.about, unsafe_allow_html=True)
+        
+
+page = sidebar()
 
 if page == "Explore":
   show_explore_page()
