168c59a87dfb8db332c0d3b85fba959c8bb5b5f0
jiunshen0918 <79739744+jiunshen0918@users.noreply.github.com>
2022-08-04 09:49:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from explore_page import show_explore_page
 from predict_page import show_predict_page
 
 def sidebar():
-  st.sidebar.image("/home/rzwitch/Downloads/randy-streamlit.png", use_column_width=True)
+  st.sidebar.image("Excited_Hedgehog.jpg", use_column_width=True)
   page = st.sidebar.selectbox("Explore or Predict", ("Explore","Predict"))
   return page
 
