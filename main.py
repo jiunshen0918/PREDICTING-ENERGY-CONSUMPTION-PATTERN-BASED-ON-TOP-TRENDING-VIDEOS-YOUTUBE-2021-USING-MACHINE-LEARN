490df27bd380e104f84e6dd047d7dfc3ae5f7c79
jiunshen0918 <79739744+jiunshen0918@users.noreply.github.com>
2022-08-04 09:47:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from explore_page import show_explore_page
 from predict_page import show_predict_page
 
+page = st.sidebar.image("/home/rzwitch/Downloads/randy-streamlit.png", use_column_width=True)
 page = st.sidebar.selectbox("Explore or Predict", ("Explore","Predict"))
 
 if page == "Explore":
