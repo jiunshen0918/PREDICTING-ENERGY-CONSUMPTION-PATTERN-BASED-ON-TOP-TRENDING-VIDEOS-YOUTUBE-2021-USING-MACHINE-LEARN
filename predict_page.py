f61b0765459b13a43549c03b6d2d87a719fd7e33
jiunshen0918 <79739744+jiunshen0918@users.noreply.github.com>
2022-08-04 08:02:17 +0800

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -73,7 +73,6 @@ def show_predict_page():
     
     # Calculation
     pred_view = st.slider('Enter the numeber of views', min_value=0, max_value=15000000, value=7500000, step=10000)
-    st.write("Or with type in:",st.text_input())
     pred_energy = calculation(pred_view)
     st.markdown(f"""Estimated energy consume for the total view is : {pred_energy:.0f} kJ""") 
 show_predict_page()
