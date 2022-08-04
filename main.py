363aabb6da66aa3e56aa205e4a1294349e83fd1b
jiunshen0918 <79739744+jiunshen0918@users.noreply.github.com>
2022-08-04 09:32:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 from explore_page import show_explore_page
 from predict_page import show_predict_page
 
+backgroundColor="#002b36"
+
 page = st.sidebar.selectbox("Explore or Predict", ("Explore","Predict"))
 
 if page == "Explore":
