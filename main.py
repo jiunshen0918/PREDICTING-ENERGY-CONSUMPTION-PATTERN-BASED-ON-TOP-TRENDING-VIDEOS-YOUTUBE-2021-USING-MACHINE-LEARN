1d01ae96e25dcad16ba08d9ce1aa1f4b096437e0
jiunshen0918 <79739744+jiunshen0918@users.noreply.github.com>
2022-08-04 09:56:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import streamlit as st
 from explore_page import show_explore_page
 from predict_page import show_predict_page
 
+st.markdown(
+    """
+<style>
+.sidebar .sidebar-content {
+    background-image: linear-gradient(#2e7bcf,#2e7bcf);
+    color: white;
+}
+</style>
+""",
+    unsafe_allow_html=True,
+)
+
 def sidebar():
   page = st.sidebar.selectbox("Explore or Predict", ("Explore","Predict"))
   st.sidebar.image("bongo-cat.gif")
