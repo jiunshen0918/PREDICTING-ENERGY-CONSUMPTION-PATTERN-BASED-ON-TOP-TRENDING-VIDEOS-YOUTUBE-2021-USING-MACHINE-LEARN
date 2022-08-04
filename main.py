c9bf5ce0b78e5f8324d13e0fb05add65102a6a6f
jiunshen0918 <79739744+jiunshen0918@users.noreply.github.com>
2022-08-04 10:13:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from explore_page import show_explore_page
 from predict_page import show_predict_page
 
 def sidebar():
-  page = st.sidebar.selectbox("Explore or Predict", ("Explore","Predict"))
+  page = st.sidebar.selectbox("Explore or Predict the energy.", ("Explore","Predict"))
   st.sidebar.image("bongo-cat.gif")
   return page
 
